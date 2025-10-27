c5355c04a15e999e62badbf6a4f87ef85e9e0f14
NicolasPazdiora <nicolas.maehler@escola.pr.gov.br>
2025-10-27 14:53:13 -0300

    adição de script

---
+++ script.js
@@ -0,0 +1,14 @@
+const avanca = document.querySelectorAll('.btn-proximo')
+
+avanca.forEach(button => {
+  button.addEventListener('click', function(){
+     cons atual = document.querySelector('.ativo');
+     cons proximoPasso = 'passo-' +
+
+
+
+
+  }
+
+
+}
