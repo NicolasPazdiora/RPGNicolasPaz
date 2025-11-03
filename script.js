aa233f58c33d9dad02b74e0777727e86563fc4a9
NicolasPazdiora <nicolas.maehler@escola.pr.gov.br>
2025-11-03 14:03:54 -0300

    adição de codigos 03/10

--- script.js
+++ script.js
@@ -2,13 +2,10 @@ const avanca = document.querySelectorAll('.btn-proximo')
 
 avanca.forEach(button => {
   button.addEventListener('click', function(){
-     cons atual = document.querySelector('.ativo');
-     cons proximoPasso = 'passo-' +
+     const atual = document.querySelector('.ativo');
+     const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
-
-
-
-  }
-
-
-}
+     atual.classList.remove('ativo');
+     document.getElementById(proximoPasso).classList.add('ativo');
+  })
+})
