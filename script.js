525367fb5263270c6bfb824a3c2e0150e1bd87d0
NicolasPazdiora <nicolas.maehler@escola.pr.gov.br>
2025-11-03 14:38:55 -0300

    ..

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-const avanca = document.querySelectorAll('.btn-proximo');
+document.addEventListener("DOMContentLoaded", () => {
+  const botoes = document.querySelectorAll(".btn-proximo");
 
-avanca.forEach(button => {
-    button.addEventListener('click', function(){
-        const atual = document.querySelector('.ativo');
-        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+  botoes.forEach(botao => {
+    botao.addEventListener("click", () => {
+      const proximoId = botao.getAttribute("data-proximo");
+      const passoAtual = botao.parentNode;
+      const proximoPasso = document.getElementById(`passo-${proximoId}`);
 
-        atual.classList.remove('ativo');
-        document.getElementById(proximoPasso).classList.add('ativo');
-    })
-})
+      // Esconde passo atual
+      passoAtual.classList.remove("ativo");
+
+      // Mostra o próximo
+      if (proximoPasso) {
+        proximoPasso.classList.add("ativo");
+      }
+    });
+  });
+});
